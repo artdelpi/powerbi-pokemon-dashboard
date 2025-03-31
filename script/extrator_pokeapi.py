a5101c5f11cba074d157c181ba32700faf059feb
artdelpi <artdelpi@gmail.com>
2025-03-31 13:24:16 -0300

    feat: normalização de tabelas + scripts de transformação

--- script/extrator_pokeapi.py
+++ script/extrator_pokeapi.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import requests
 import pandas as pd
 
@@ -8,23 +7,19 @@ url = "https://pokeapi.co/api/v2/pokemon?limit=500"
 # Converte a resposta em dicionário
 response = requests.get(url).json()
 pokemon_list = response['results']
-
-# Lista para armazenar os dados
 data = []
 
 # Loop para pegar os detalhes de cada Pokémon
 for pokemon in pokemon_list:
     details = requests.get(pokemon['url']).json()
-    
-    # Obtém todos os atributos importantes
     data.append({
         'ID': details['id'],
         'Nome': details['name'],
         'Altura': details['height'],
         'Peso': details['weight'],
         'Base XP': details['base_experience'],
-        'Tipos': ', '.join(t['type']['name'] for t in details['types']),  # Lista de tipos
-        'Habilidades': ', '.join(a['ability']['name'] for a in details['abilities']),  # Lista de habilidades
+        'Tipos': ', '.join(t['type']['name'] for t in details['types']),
+        'Habilidades': ', '.join(a['ability']['name'] for a in details['abilities']),
         'HP': details['stats'][0]['base_stat'],
         'Ataque': details['stats'][1]['base_stat'],
         'Defesa': details['stats'][2]['base_stat'],
@@ -34,6 +29,4 @@ for pokemon in pokemon_list:
 
 # Cria DataFrame
 pokemon_bruto = pd.DataFrame(data)
-
-# Retorna o DataFrame
 pokemon_bruto
